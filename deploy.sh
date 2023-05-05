46714b37e3610b41c8327f5db0f14a82b1f43d78
Ghabryel <ghabryelhenrique@hotmail.com>
2023-05-05 17:53:41 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Building Docker image..."
 docker build -t eventos-tickets .
 
 echo "Pushing Docker image to Docker Hub..."
-docker push eventos-tickets
+docker push ghabryel/eventos-tickets
 
 echo "Deploying Docker container to Hostinger VPS..."
 ssh root@181.215.134.104 "cd Amg-ingressos-aqui-eventos-api && docker-compose pull && docker-compose up -d"
