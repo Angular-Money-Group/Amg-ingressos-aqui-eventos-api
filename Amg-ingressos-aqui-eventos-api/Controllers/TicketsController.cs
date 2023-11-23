ba23d811f9f6b5f9f3ddf76b879a74c1af908cb6
roberthsx <roberthenrrique@gmail.com>
2023-11-23 11:17:01 -0300

    retirada de pasta binaria e ajuste de controllers

--- Amg-ingressos-aqui-eventos-api/Controllers/TicketsController.cs
+++ Amg-ingressos-aqui-eventos-api/Controllers/TicketsController.cs
@@ -2,7 +2,6 @@ using Amg_ingressos_aqui_eventos_api.Consts;
 using Amg_ingressos_aqui_eventos_api.Dto;
 using Amg_ingressos_aqui_eventos_api.Exceptions;
 using Amg_ingressos_aqui_eventos_api.Model;
-using Amg_ingressos_aqui_eventos_api.Model.Querys;
 using Amg_ingressos_aqui_eventos_api.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
