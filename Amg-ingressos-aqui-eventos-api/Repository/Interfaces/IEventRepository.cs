80b0c11a7aa648c9319c0ead619fbfba9c237703
roberthsx <roberthenrrique@gmail.com>
2023-12-18 14:00:47 -0300

    ajuste filtro de eventos

--- Amg-ingressos-aqui-eventos-api/Repository/Interfaces/IEventRepository.cs
+++ Amg-ingressos-aqui-eventos-api/Repository/Interfaces/IEventRepository.cs
@@ -1,3 +1,4 @@
+using Amg_ingressos_aqui_eventos_api.Enum;
 using Amg_ingressos_aqui_eventos_api.Model;
 
 namespace Amg_ingressos_aqui_eventos_api.Repository.Interfaces
@@ -10,7 +11,7 @@ namespace Amg_ingressos_aqui_eventos_api.Repository.Interfaces
         Task<List<Event>> GetByProducer<T>(string id, Pagination paginationOptions, FilterOptions? filterOptions);
         Task<List<Event>> GetByName<T>(string name);
         Task<object> Delete<T>(object id);
-        Task<List<T>> GetAllEvents<T>(Pagination paginationOptions);
+        Task<List<T>> GetAllEvents<T>(Pagination paginationOptions, Event eventModel);
         Task<List<T>> GetAllEventsWithTickets<T>(string idEvent, string idOrganizer);
         Task<List<T>> GetAllEventsWithTransactions<T>(string idEvent, string idOrganizer);
         Task<Event> Edit<T>(string id, Event eventObj);
