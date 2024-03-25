e7cd4b6a403d2c040e4e0e84f6132c9d43e9d3e3
roberthsx <roberthenrrique@gmail.com>
2024-03-25 18:01:10 -0300

    ajuste de deploy

--- start-process.sh
+++ start-process.sh
@@ -11,6 +11,7 @@ docker pull 590184025050.dkr.ecr.us-east-1.amazonaws.com/events-api-prod:latest
 docker image tag 590184025050.dkr.ecr.us-east-1.amazonaws.com/events-api-prod:latest $container_name:latest
 
 #stop and remove the current container docker rm -f $container_name
-
+docker stop $container_name
+docker rm $container_name
 #Creating and starting a docker container using a new image
 docker run -d -p 3001:80 --name $container_name $container_name:latest
